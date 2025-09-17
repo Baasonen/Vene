3448d3aef41c6eeae52370b47ba61aa6dd54676b
Baasonen <hejupa035@gmail.com>
2025-09-17 16:13:36 +0300

    Update V2.ino

--- Vene/V2/V2.ino
+++ Vene/V2/V2.ino
@@ -14,11 +14,12 @@ IPAddress lastIP;
 
 unsigned long TXRate = 1;
 
+unsigned char MODE = 0;
 // Incomming Packets BhBBBh
 // Outgoing Packets BhhhBBB
 
 #pragma pack(push, 1)
-struct Control_Packet 
+struct ControlPacket 
 {
   unsigned char mode;
   unsigned short int heading;
@@ -30,7 +31,7 @@ struct Control_Packet
 #pragma pop
 
 #pragma pack(push, 1)
-struct Telemetry_packet
+struct TelemetryPacket
 {
   unsigned char mode;
   unsigned short int heading;
@@ -43,11 +44,12 @@ struct Telemetry_packet
 };
 #pragma pop
 
-Control_Packet inbound;
-Telemetry_packet outbound;
+ControlPacket inbound;
+TelemetryPacket outbound;
 
 unsigned long TXRMillis = 1000.0 / TXRate;
 unsigned long lastTelemetryTime = 0;
+
 
 void setup() 
 {
@@ -68,16 +70,10 @@ void loop()
 {
   // Check for inbound packet
   int packetSize = udp.parsePacket();
-  if (packetSize == sizeof(Control_Packet)) {
-    udp.read((uint8_t*)&inbound, sizeof(Control_Packet)); // directly cast buffer to struct
+  if (packetSize == sizeof(ControlPacket)) {
+    udp.read((uint8_t*)&inbound, sizeof(ControlPacket)); // directly cast buffer to struct
 
-    lastIP = udp.remoteIP(); // Save sender ip
-
-    Serial.println("Mode: "); Serial.print(inbound.mode);
-    Serial.print(" | Steering: "); Serial.print(inbound.heading);
-    Serial.print(" | Throttle1: "); Serial.print(inbound.throttle1);
-    //Serial.print(" | Throttle2: "); Serial.print(inbound.throttle2);
-    //Serial.print(" | Timestamp: "); Serial.println(inbound.timestamp);
+    lastIP = udp.remoteIP(); // Save sender ip  
   }
 
   // Send telemetry
@@ -86,9 +82,9 @@ void loop()
     lastTelemetryTime = millis();
 
     double t = millis() / 1000.0;
-    outbound.mode = inbound.mode;
+    outbound.mode = MODE;
     outbound.heading = (unsigned short)(sin(t) * 180 + 180);
-    outbound.speed = (unsigned short)(abs(sin(t)) * 100);
+    outbound.speed = (unsigned short)(sin(t) * 180 + 180);
     outbound.tilt = (unsigned short)(sin(t) * 45 + 45);
     outbound.gpsLat = 1111;
     outbound.gpsLon = 2222;
@@ -96,9 +92,12 @@ void loop()
     outbound.error = 0;
 
     udp.beginPacket(lastIP, TXPort);
-    udp.write((uint8_t*)&outbound, sizeof(Telemetry_packet));
+    udp.write((uint8_t*)&outbound, sizeof(TelemetryPacket));
     udp.endPacket();
 
     //Serial.println("Telemetry Sent");
   }
+
+
+  // Logiikka tänne ??
 }
