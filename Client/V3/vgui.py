34158c73f9fbdd876d22eba0107500d1fa3d76fd
onni-2 <onnip@protonmail.com>
2025-10-06 10:49:02 +0300

    Added vgui

---
+++ Client/V3/vgui.py
@@ -0,0 +1,361 @@
+import tkinter as tk
+from tkinter import ttk
+import tkinter.font as tkFont
+import tkintermapview
+from time import strftime
+import os
+import pygame
+from vcom import Vene
+
+
+class VeneGui(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Vene Gui V2')
+
+        self.width = int(self.winfo_screenwidth() / 1.5)
+        self.height = int(self.winfo_screenheight() / 1.5)
+        self.geometry(f'{self.width}x{self.height}')
+
+        self.bg_color = '#FFFFFF'
+        self.configure(bg=self.bg_color)
+        self.default_font = tkFont.nametofont("TkDefaultFont")
+        self.default_font.configure(family="Inter", size=10, weight="normal")
+
+        self.style = ttk.Style(self)
+        self.style.configure("Custom.TFrame", background=self.bg_color)
+        self.style.configure("Custom.TLabel", background=self.bg_color)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=2)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        # Create frames
+        self.statusframe = StatusFrame(self)
+        self.statusframe.grid(row=0, column=0, sticky="nsew")
+
+        self.mapframe = MapFrame(self)
+        self.mapframe.grid(row=0, column=1, sticky="nsew")
+
+        self.waypointframe = WaypointFrame(self)
+        self.waypointframe.grid(row=0, column=2, sticky="nsew")
+
+        self.wp_list = []
+
+        self.mapframe.offline_map.add_right_click_menu_command(
+            label="Add waypoint",
+            command=self.add_waypoint,
+            pass_coords=True
+            
+        )
+
+        self.waypointframe.update_wp_gui(self.wp_list, self.mapframe)
+        self.waypointframe.update_time()
+        self.after(1000, self.periodic_update)
+
+
+    def add_waypoint(self, coords):
+        print("Add waypoint:", coords)
+        self.wp_list.append(coords)
+        self.waypointframe.update_wp_gui(self.wp_list, self.mapframe)
+
+
+    def periodic_update(self):
+        self.waypointframe.update_time()
+        self.after(1000, self.periodic_update)
+
+class StatusFrame(ttk.Frame):  # Kartan vasen puoli
+    def __init__(self, container):
+        super().__init__(container, style="Custom.TFrame")
+        self.bg_color = container.bg_color
+
+        self.label_wp = tk.Label(self, text="Vene Status:", font=("Inter", 17, "bold"), bg=container.bg_color)
+        self.label_wp.pack(side="top", anchor="w", padx=20, pady=10)
+
+        self.boat = Vene()
+
+        self.receive_label = tk.Label(self, text="Received from Vene:", font=("Inter", 10, "bold"), bg=container.bg_color)
+        self.receive_label.pack(side="top", anchor="w", padx=30, pady=(30,0))
+
+        self.telemetry_frame = tk.Frame(self,  bg=container.bg_color)
+        self.telemetry_labels = {}
+        self.telemetry_vars = {
+            "t_mode": "Mode",
+            "t_heading": "Heading",
+            "t_speed": "Speed",
+            "t_coords": "Coordinates",
+            "t_battery": "Battery",
+            "t_target_wp": "Target waypoint",
+            "t_gps_status": "GPS status",
+            "t_gen_errror": "Gen error",
+            "t_packets_per_second": "Packets per second"
+#            "_initialized": "Vcom initialized"
+        }
+
+
+        for i, var in enumerate(self.telemetry_vars):
+            lbl = tk.Label(self.telemetry_frame, text=f"{var}: ---", bg=container.bg_color)
+            lbl.grid(row=i, column=0, sticky="w")
+            self.telemetry_labels[var] = lbl
+        self.telemetry_frame.pack(side="top", anchor="w", padx=60, pady=10)
+
+        self.send_label = tk.Label(self, text="Sending to Vene:", font=("Inter", 10, "bold"), bg=container.bg_color)
+        self.send_label.pack(side="top", anchor="w", padx=30, pady=(30,0))
+
+        self.controls_frame = tk.Frame(self, bg=container.bg_color)
+        self.control_labels = {}
+        control_vars = ["mode", "rudder", "throttle", "light_mode"]
+        for i, var in enumerate(control_vars):
+            lbl = tk.Label(self.controls_frame, text=f"{var}: ---", bg=container.bg_color)
+            lbl.grid(row=i, column=0, sticky="w")
+            self.control_labels[var] = lbl
+        self.controls_frame.pack(side="top", anchor="w", padx=60, pady=10)
+
+
+        ''' Tarkistaa SSID:n käynnistyessä, hidastaa käynnistymistä, eikä elä toimi Windowsilla vielä
+        def ssid():
+            try:
+                ssid_output = os.popen("nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2").read().strip()
+                if len(ssid_output) < 15:
+                    print(ssid_output)
+                    return ssid_output
+                else:
+                    return "None"
+            except Exception:
+                return "None"
+            
+    
+        self.send_label = tk.Label(self, text=f"Connected to {ssid()}", font=("Inter", 10, "bold"), bg=container.bg_color)
+        self.send_label.pack(side="top", anchor="w", padx=30, pady=(30,0))
+        '''
+
+        self.controller_frame = ControllerFrame(self)
+        self.controller_frame.pack(side="top", anchor="w", padx=40, pady=(60,40))
+        
+        tk.Button(
+            self,
+            text="Start vcom",
+            command=lambda: self.boat.start(),
+            bg="#9af97a"
+            ).pack(anchor="w", padx=40)
+        
+        tk.Button(
+            self,
+            text="Stop vcom",
+            command=lambda: self.boat.shutdown(),
+            bg="#ffcdcc"
+            ).pack(anchor="w", padx=40, pady=10)
+    
+        self.update_gui()
+    
+    def update_gui(self):
+        for var, lbl in self.telemetry_labels.items():
+            display_name = self.telemetry_vars.get(var, var)
+            lbl.config(text=f"{display_name}: {getattr(self.boat, var)}")
+
+        for var, lbl in self.control_labels.items():
+            lbl.config(text=f"{var}: {getattr(self.boat, var)}")
+
+        self.after(200, self.update_gui) 
+
+class WaypointFrame(ttk.Frame):  # Kartan oikea puoli
+    def __init__(self, container):
+        super().__init__(container, style='Custom.TFrame')
+        self.container = container
+
+
+        self.wp_label = ttk.Label(self, text="Waypoints: ", style='Custom.TLabel')
+        self.wp_label.pack(pady=(10,5))
+
+        self.wp_gui = tk.Listbox(self, height=15, font=("Inter", 10))
+        self.wp_gui.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+
+        tk.Button(
+            self,
+            text="Clear all waypoints",
+            command=lambda: self.empty_wp(),
+            bg="#ffcdcc"
+            ).pack()
+
+        tk.Button(
+            self,
+            text="Send waypoints to Vene",
+            command=lambda: self.empty_wp(),
+            bg="#9af97a"
+            ).pack(pady=5)
+
+        self.time_label = ttk.Label(self, text="Choose Mode: ", style='Custom.TLabel')
+        self.time_label.pack(pady=(30, 5))
+
+        self.boat = Vene() 
+        self.mode = tk.IntVar(value=0)
+
+        self.modes = ("0", "1", "2", "3", "4", "5")
+
+        for mode in self.modes:
+            r = ttk.Radiobutton(
+                self,
+                text=mode,
+                value=int(mode),
+                variable=self.mode)
+            r.pack(fill='x', padx=60, pady=5)
+
+        tk.Button(
+            self,
+            text="Send mode",
+            command=lambda: self.boat.set_control(mode=self.mode.get()),
+            bg="#9af97a"
+            ).pack()
+
+        self.clock_label = ttk.Label(self, text="00:00:00", font=("Inter", 12, "normal"), style='Custom.TLabel')
+        self.clock_label.pack(anchor="e", padx=10, pady=(10, 15))
+
+    def update_time(self):
+        current_time = strftime('%H:%M:%S')
+        self.clock_label.config(text=current_time)
+
+    def empty_wp(self):
+        self.wp_gui.delete(0, tk.END)
+        self.container.wp_list.clear()
+        self.container.mapframe.offline_map.delete_all_marker()
+
+    def update_wp_gui(self, wp_list, mapframe):
+        self.wp_gui.delete(0, tk.END)
+        for idx, wp in enumerate(wp_list, start=1):
+            self.wp_gui.insert(tk.END, f"{idx}: ({wp[0]:.4f}, {wp[1]:.4f})")
+            mapframe.wp_on_map(wp)
+
+class MapFrame(tk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        self.top_left_position = (60.6479716, 24.0170517)   #(60.19711, 24.81159)
+        self.bottom_right_position = (59.9924890, 25.5054310)  #(60.18064, 24.85399)
+        self.zoom_min = 0
+        self.zoom_max = 15
+        self.script_directory = os.path.dirname(os.path.abspath(__file__))
+        self.database_path = os.path.join(self.script_directory, "offline_tiles.db")
+
+        self.loader = tkintermapview.OfflineLoader(
+            path=self.database_path,
+            tile_server="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        )
+        # Lataa offline-kartan
+        # self.loader.save_offline_tiles(self.top_left_position, self.bottom_right_position, self.zoom_min, self.zoom_max)
+
+        self.offline_map = tkintermapview.TkinterMapView(
+            self,
+            width=600,
+            height=600,
+            use_database_only=True,
+            max_zoom=18,
+            database_path=self.database_path
+        )
+
+        self.boat = Vene()
+
+        self.offline_map.set_position(60.185921, 24.825963) # Otaniemi, myös self.boat.t_coords[0], self.boat.t_coords[1]
+        self.offline_map.set_zoom(15)
+        self.offline_map.pack(fill=tk.BOTH, expand=True)
+
+        
+
+        self.vene_marker = self.offline_map.set_marker(self.boat.t_coords[0], self.boat.t_coords[1], text=f"Vene: {self.boat.t_coords}")
+
+    def move_vene(self):
+        new_lat = self.boat.t_coords[0]
+        new_lon = self.boat.t_coords[1]
+        self.vene_marker.set_position(new_lat, new_lon)
+        self.vene_marker.set_text(f"Vene: {self.boat.t_coords}")
+
+        self.after(100, self.move_vene())
+
+    def wp_on_map(self, wp):
+        self.offline_map.set_marker(wp[0], wp[1], text=f"({wp[0]:.5f}, {wp[1]:.5f})")
+
+class ControllerFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container, style="Custom.TFrame")
+        self.bg_color = container.bg_color
+
+        self.controller = Controller()
+
+        self.controller_status = tk.StringVar(value="no_value")
+
+        self.controller_status_label = tk.Label(self, textvariable=self.controller_status, bg=self.bg_color)
+        self.controller_status_label.pack(anchor="w")
+
+
+        
+        self.canvas = tk.Canvas(self, width=300, height=200, bg="white")
+        self.canvas.pack(anchor="w")
+
+        self.lx_line = self.canvas.create_line(0, 50, 150, 50, width=4, fill="blue")
+        self.thr_line = self.canvas.create_line(0, 150, 150, 150, width=4, fill="green")
+
+        self.steer_center = 150
+        self.steer_length = 100
+
+
+        self.update_controller()
+        self.update_lines()
+        self.controller.poll_joystick(container)
+
+
+    def update_controller(self): # ei toimi
+        if self.controller.joystick is None:
+            self.controller_status.set("No controller detected")
+        else:
+            self.controller_status.set("Controller connected")
+    
+        self.after(200, self.update_controller)
+
+    def update_lines(self):
+        lx_offset = self.controller.axis0 * self.steer_length
+        
+        if lx_offset >= 0:
+            self.canvas.coords(self.lx_line, self.steer_center, 50, self.steer_center + lx_offset, 50)
+        else:
+            self.canvas.coords(self.lx_line, self.steer_center + lx_offset, 50, self.steer_center, 50)
+
+
+        thr_norm = (self.controller.axis5 + 1) / 2 
+        thr_x = 50 + thr_norm * 200
+        self.canvas.coords(self.thr_line, 50, 150, thr_x, 150)
+
+        self.after(50, self.update_lines)
+
+class Controller:
+    def __init__(self,):
+        pygame.init()
+        pygame.joystick.init()
+        if pygame.joystick.get_count() == 0:
+            print("No controller detected")
+            self.joystick = None
+        else:
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+            print(f"Controller connected: {self.joystick.get_name()}")
+
+        self.boat = Vene()
+        self.axis0 = 0
+        self.axis5 = 0
+        
+
+    def poll_joystick(self, root):
+        if self.joystick:
+            pygame.event.pump()
+            self.axis0 = self.joystick.get_axis(0)
+            self.boat.set_control(rudder=int((self.axis0 + 1) * 90))
+            self.axis5 = self.joystick.get_axis(5)
+            self.boat.set_control(throttle=int((self.axis5 + 1) * 50))
+        else:
+            self.axis0 = 0
+            self.axis5 = 0
+
+        root.after(50, self.poll_joystick, root)
+ 
+if __name__ == "__main__":
+    app = VeneGui()
+    app.mainloop()
