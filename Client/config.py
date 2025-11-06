3e18e13b7747b558ff506bee22e4d731245dc8c8
onni-2 <onnip@protonmail.com>
2025-11-07 00:36:24 +0200

    Add files via upload

---
+++ Client/config.py
@@ -0,0 +1,36 @@
+# Asetukset
+
+DEBUG_MODE = 1
+FULLSCREEN = True
+
+LIGHT_THEME = {
+    "bg": "#ffffff",
+    "fg": "#000000",
+    "button_bg": "#f0f0f0",
+    "entry_bg": "#ffffff"
+}
+
+DARK_THEME = {
+    "bg": "#232327",
+    "fg": "#FFFFFF",
+    "button_bg": "#333237",
+    "entry_bg": "#333237"
+}
+
+MAP_CONFIG = {
+    "default_position": (60.185921, 24.825963),
+    "default_zoom": 15,
+    "offline_db": "offline_tiles.db", #jos haluaa käyttä jotain muuta, esim. ladata satelliittikuvan
+    #Offline downloading
+    "loader_enabled": False,  #käytä vain jos tarvii ladata lisää karttaa, aseta parametrit alla
+    "top_left": (60.1485939, 24.7293509), #(60.19711, 24.81159)
+    "bottom_right": (60.1411166, 24.7526110), #(60.18064, 24.85399)
+    "zoom_min": 0,
+    "zoom_max": 17, #Varovasti, zoom_level 20:n mittakaava on jo 1:500.
+    "tile_server": "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+}
+
+CONTROLLER_CONFIG = {
+    "deadzone": 0.07,  # 0.00 - 1.00
+    "poll_interval": 50
+}
