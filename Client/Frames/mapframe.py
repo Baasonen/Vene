c41d5ab47e479a3c25141dddc58ce7ec96bc43e5
onni-2 <onnip@protonmail.com>
2025-11-07 02:17:10 +0200

    Improve wp and path design

--- Client/Frames/mapframe.py
+++ Client/Frames/mapframe.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import config
 import tkintermapview
 
 class MapFrame(ttk.Frame):
